cab69acaa4356a35cdce9be62805bdb9af76b468
Anton Adamansky <adamansky@gmail.com>
2023-01-22 12:08:10 +0200

    * cmake build script fixes

--- cmake/Modules/AddIOWOW.cmake
+++ cmake/Modules/AddIOWOW.cmake
@@ -1,6 +1,10 @@
-if(TARGET IOWOW::static) 
+find_package(IOWOW)
+
+if(TARGET IOWOW::static)
   return()
 endif()
+
+set(IOWOW_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/include)
 
 include(ExternalProject)
 
@@ -13,19 +17,15 @@ set(BYPRODUCT "${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/libiowow-1.a")
 
 set(CMAKE_ARGS
     -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-    -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR} 
-    -DBUILD_SHARED_LIBS=OFF
+    -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR} -DBUILD_SHARED_LIBS=OFF
     -DBUILD_EXAMPLES=OFF)
 
-set(CMAKE_FIND_ROOT_PATH ${CMAKE_FIND_ROOT_PATH} ${CMAKE_INSTALL_PREFIX})
+set(CMAKE_FIND_ROOT_PATH ${CMAKE_FIND_ROOT_PATH} ${CMAKE_INSTALL_PREFIX}
+                         ${CMAKE_BINARY_DIR})
 
-# In order to properly pass owner project CMAKE variables than contains
-# semicolons, we used a specific separator for 'ExternalProject_Add', using the
-# LIST_SEPARATOR parameter. This allows building fat binaries on macOS, etc.
-# (ie: -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64") Otherwise, semicolons get
-# replaced with spaces and CMake isn't aware of a multi-arch setup.
-set(SSUB "^^")
+list(REMOVE_DUPLICATES CMAKE_FIND_ROOT_PATH)
 
+set(SSUB "|")
 foreach(
   extra
   ANDROID_ABI
@@ -42,8 +42,6 @@ foreach(
   PLATFORM
   TEST_TOOL_CMD)
   if(DEFINED ${extra})
-    # Replace occurences of ";" with our custom separator and append to
-    # CMAKE_ARGS
     string(REPLACE ";" "${SSUB}" extra_sub "${${extra}}")
     list(APPEND CMAKE_ARGS "-D${extra}=${extra_sub}")
   endif()
@@ -58,18 +56,17 @@ ExternalProject_Add(
   TIMEOUT 360
   PREFIX ${CMAKE_BINARY_DIR}
   BUILD_IN_SOURCE OFF
-  #DOWNLOAD_EXTRACT_TIMESTAMP ON
+  # DOWNLOAD_EXTRACT_TIMESTAMP ON
   UPDATE_COMMAND ""
-  LIST_SEPARATOR "${SSUB}"
   CMAKE_ARGS ${CMAKE_ARGS}
+  LIST_SEPARATOR "${SSUB}"
   BUILD_BYPRODUCTS ${BYPRODUCT})
 
 add_library(IOWOW::static STATIC IMPORTED GLOBAL)
 set_target_properties(
   IOWOW::static
   PROPERTIES IMPORTED_LINK_INTERFACE_LANGUAGES "C"
-             IMPORTED_LOCATION
-             "${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/libiowow-1.a"
+             IMPORTED_LOCATION ${BYPRODUCT}
              IMPORTED_LINK_INTERFACE_LIBRARIES "Threads::Threads;m")
 
 add_dependencies(IOWOW::static extern_iowow)
